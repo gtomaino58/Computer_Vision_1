1e0ddd115495b437f94994042d9409d09777a0bd
gtomaino58 <gtomaino@hotmail.com>
2025-04-11 19:01:55 +0200

    Second commit 2025-10-04 12:00:00

--- cancer_de_piel_test.py
+++ cancer_de_piel_test.py
@@ -13,11 +13,18 @@ import matplotlib.pyplot as plt
 # Vamos a cargar el archivo de datos de las imagenes 
 #C:\Users\gtoma\Downloads\VisArt\skin_cancer_dataset\train-20240324T151905Z-001\train\benign
 #C:\Users\gtoma\Downloads\VisArt\skin_cancer_dataset\train-20240324T151905Z-001\train\malignant
+
 # Vamos a cargar el archivo de datos de las imagenes
+#C:\Users\gtoma\Downloads\VisArt\Computer_Vision_1\train\benign
+#C:\Users\gtoma\Downloads\VisArt\Computer_Vision_1\train\malignant
+#C:\Users\gtoma\Downloads\VisArt\Computer_Vision_1\test\benign
+#C:\Users\gtoma\Downloads\VisArt\Computer_Vision_1\test\malignant
 # Cargamos las librerias necesarias
 
+
+
 # Definimos la ruta de los datos
-data_dir = r'C:\Users\gtoma\Downloads\VisArt\skin_cancer_dataset\train-20240324T151905Z-001\train'
+data_dir = r'C:\Users\gtoma\Downloads\VisArt\Computer_Vision_1\train'
 
 # Definimos la ruta de los datos de las imagenes benignas y malignas
 benign_dir = os.path.join(data_dir, 'benign')
@@ -30,6 +37,7 @@ malignant_images = os.listdir(malignant_dir)
 # Vamos a cargar las imagenes benignas y malignas
 benign_images = [os.path.join(benign_dir, img) for img in benign_images]
 malignant_images = [os.path.join(malignant_dir, img) for img in malignant_images]
+
 benign_images
 malignant_images
 
@@ -62,5 +70,17 @@ plt.axis('off')
 plt.subplot(1, 2, 2)
 plt.title('Malignant Image')
 plt.imshow(malignant_images[0])
+plt.axis('off')
+plt.show();
+
+# Vamos a ver las imagenes de test
+plt.figure(figsize=(10, 10))
+plt.subplot(1, 2, 1)
+plt.title('Benign Image')
+plt.imshow(benign_images[1])
+plt.axis('off')
+plt.subplot(1, 2, 2)
+plt.title('Malignant Image')
+plt.imshow(malignant_images[1])
 plt.axis('off')
 plt.show();
